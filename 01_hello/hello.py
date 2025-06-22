75e3e4186dba1f0af6ecf307d6f224b3da729a17
Fábio Soriano <fabio_vsr@yahoo.com.br>
2025-06-22 17:32:02 +0000

    Salvar alterações do Codespaces

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,1 +1,16 @@
+# Este programa imprime "Hello, world!" na saída
+# Função print
+print("Hello, world!")
 
+# Para iniciar o programa seria correto fazer:
+# python hello.py
+# mas o Codespaces não permite isso.
+# A palavra "python" é o comando que roda o interpretador escrito em Python,
+# mas no Codespaces do Github basta clicar em Play, acima, do lado direito. 
+
+# Argument: argumento é a entrada de dados em uma função que influencia seu comportamento.
+# Neste caso a string de testa que se deseja que seja impressa.
+
+# Side effect: efeito colateral, é quando uma função altera algo fora de si mesma,
+# em vez de apenas retornar um valor.
+# Neste caso a função print imprime uma string no terminal.
---
+++ 02_hello_name/hello_name.py
@@ -0,0 +1,30 @@
+# Este programa pergunta seu nome,
+# e depois imprime "Hello, " junto com seu nome na saída.
+
+# Escrever alguma coisa direto no programa, assim
+# print("Hello, fvsr") se chama em inglês "to hardcode your name",
+# e não é isso que queremos.
+
+# Return Values: quando enviamos um dados para uma função
+# (caso ela não seja uma função void), ela faz uma cálculo
+# e te retorna um valor
+
+# Variables: são as váriaveis, um espaço de memória reservado no computador
+# que tem a função de armazenar um valor.
+
+# Assignment operator: é o operador de atribuição, sinal =,
+# que faz com que o valor captado pela função da direita
+# seja escrito na variável a esquerda.
+# É importante lembrar que o movimento da informação na atribuição é da direita para esquerda.
+
+# Pseudocode: é o pseudocódigo, que são tarefas descritas em linguagem comum
+# para serem postariormente escritas em linguagem de programação.
+
+# pergunta ao usuário seu nome (exemplo de pseudocode)
+name = input("What's your name? ") # Observar o espaço depois do sinal de interrogação.
+
+# Diz hello para o usuário (exemplo de pseudocode)
+print("Hello, " + name) # Observar o espaço depois da vírgula.
+
+# Podemos escrever a linha de código anterior deste jeito também
+print("Hello,",name) #Esta linha de comando acrescenta um espaço para cada argumento depois do texto.
