8327c2375f449cb7dae72a85573b8b4cc662c728
Fábio Soriano <fabio_vsr@yahoo.com.br>
2025-06-23 20:00:56 +0000

    Salva código que exemplifica ouso do tipo float

---
+++ 06_float/float.py
@@ -0,0 +1,11 @@
+# Este código desenvolve operações com números do tipo float,
+# conjuntos dos números Racionais (Q) e Irracionais (I).
+
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+
+print(x + y)
+
+# Função de arredondamento:
+# round(number[, digits])
+
