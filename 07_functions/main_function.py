f167e0b35b87926ba8ae8c06d6fc8c2c1b7bbc9f
Fábio Soriano <fabio_vsr@yahoo.com.br>
2025-06-24 16:39:53 +0000

    Explicação sobre a função main.

---
+++ 07_functions/main_function.py
@@ -0,0 +1,13 @@
+# Este arquivo é sobre o uso da função main.
+
+# No arquivo functions.py está exemplificado que 
+# uma função em Python só pode ser chamada caso esteja completamente definida em linhas anteriores.
+# Em códigos grandes isso dificulta a leitura, pois deixa o código assim:
+"""
+def função_1()
+def função_2()
+...
+def função_n()
+código_principal
+"""
+# Para evitar isso fazemos
