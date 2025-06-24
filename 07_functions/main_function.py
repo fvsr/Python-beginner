c42124183d4f1a86f76cb72da0d25132ae6de4ea
Fábio Soriano <fabio_vsr@yahoo.com.br>
2025-06-24 17:04:22 +0000

    Função main e escopa de uma variável.

--- 07_functions/main_function.py
+++ 07_functions/main_function.py
@@ -10,4 +10,23 @@ def função_2()
 def função_n()
 código_principal
 """
-# Para evitar isso fazemos
+# Para evitar isso fazemos:
+def main(): # Deixa claro que esta é a parte principal do programa
+    hello() # Nesta chamada de função é usado o valor default que está em to="world!"
+    name = input("What's your name? ")
+    hello(name)
+
+def hello(to="world!"):
+    print("Hello,",to)
+
+main()
+
+# Este é o erro com relação ao escopo da variável:
+"""
+def main():
+    name = input("What's your name? ") # A variável "name" só é reconhecida dentro da função main.
+    hello()
+
+def hello():
+    print("Hello,",name) # A variável name não foi definida dentro desta função, vai dar erro.
+"""
