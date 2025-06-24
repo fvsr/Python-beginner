4f22d527c6afb2a9a51f4a1a131d0404caa5a3cf
Fábio Soriano <fabio_vsr@yahoo.com.br>
2025-06-24 03:59:16 +0000

    Definição de funções

---
+++ 07_functions/functions.py
@@ -0,0 +1,26 @@
+# Para definir uma função usa-se o comando "def".
+
+# Este código não vai funcionar porque a função hello() não foi definida.
+# Tire as aspas triplas e rode o programa para verificar a mensagem de erro.
+"""
+name = input("What's your name? ")
+hello()
+print(name)
+"""
+
+# Usando o mesmo código, desta vez com a função hello() sendo definida:
+"""
+def hello():
+    print("Hello!")
+
+name = input("What's your name? ")
+hello()
+print(name)
+"""
+
+# A mesma função, mas usando passagem de parâmetro:
+def hello(to):
+    print("Hello,",to)
+
+name = input("What's your name? ")
+hello(name)
