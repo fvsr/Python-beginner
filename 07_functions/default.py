4ee6ca3544a75245fbb4655bc74898a457e8a5fc
Fábio Soriano <fabio_vsr@yahoo.com.br>
2025-06-24 15:56:07 +0000

    Código do site Stack Overflow.

--- 07_functions/default.py
+++ 07_functions/default.py
@@ -1,1 +1,12 @@
-#
+# Esta questão aparece no temp 1h e 38 m do curso.
+# Verifique o código abaixo qual o valor de i que vai ser impresso:
+
+i = 5
+
+def f(arg=i):
+    print(arg)
+
+i = 6
+f()
+
+# Saída vai ser 5 porque i era igual a 5 quando a definição da função f() é lida.  
