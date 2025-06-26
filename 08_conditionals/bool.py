8116ea5d0f6eb01b3399016be4e9a5d50cfc333d
Fábio Soriano <fabio_vsr@yahoo.com.br>
2025-06-26 05:38:44 +0000

    Variável booleana

---
+++ 08_conditionals/bool.py
@@ -0,0 +1,17 @@
+# Este código exemplifica o uso de uma função que retorna um valor lógico,
+# verdadeiro ou falso, através do uso de uma variável lógica.
+
+def main():
+    x = int(input("What's x? "))
+    if is_even(x):
+        print("Even")
+    else:
+        print("Odd")
+
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+
+main()
