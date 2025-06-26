d87c649f321ce8be44e5c81a40b577d546412ec2
Fábio Soriano <fabio_vsr@yahoo.com.br>
2025-06-26 17:17:29 +0000

    Segunda versão com otimização do código.

--- 08_conditionals/match.py
+++ 08_conditionals/match.py
@@ -1,6 +1,8 @@
 # Este código exemplifica o comando "match".
 # O comando "match" é o equivalente ao comando "switch" em outras linguagens.
+# A comparação entre strings é case sensitive, isto é, maiúscula e minúscula fazem diferença.
 
+"""
 name = input("What's your name? ")
 
 if name == "Harry":
@@ -13,3 +15,15 @@ elif name == "Draco":
     print("Slytherin")
 else:
     print("Who?")
+"""
+
+# Otimizando o código anterior.
+name = input("What's your name? ")
+
+if name == "Harry" or name == "Hermione" or name == "Ron":
+    print("Gryffindor")
+elif name == "Draco":
+    print("Slytherin")
+else:
+    print("Who?")
+
