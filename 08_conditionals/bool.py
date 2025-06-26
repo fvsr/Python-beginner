19ba7705f79f7fa8f98eab870fdb32d8a0962f09
Fábio Soriano <fabio_vsr@yahoo.com.br>
2025-06-26 05:47:03 +0000

    Otimização.

--- 08_conditionals/bool.py
+++ 08_conditionals/bool.py
@@ -1,6 +1,7 @@
 # Este código exemplifica o uso de uma função que retorna um valor lógico,
 # verdadeiro ou falso, através do uso de uma variável lógica.
 
+"""
 def main():
     x = int(input("What's x? "))
     if is_even(x):
@@ -13,5 +14,19 @@ def is_even(n):
         return True
     else:
         return False
+
+main()
+"""
+
+# Em Python podemos otimizar a função is_even():
+def main():
+    x = int(input("What's x? "))
+    if is_even(x):
+        print("Even")
+    else:
+        print("Odd")
+
+def is_even(n):
+    return True if n % 2 == 0 else False # Sintaxe particular do Python.
 
 main()
