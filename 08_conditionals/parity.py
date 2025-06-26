9199142834aba192812056c5bc9bb670bf56beeb
Fábio Soriano <fabio_vsr@yahoo.com.br>
2025-06-26 05:30:06 +0000

    Exemplo de uso do operardor módulo.

---
+++ 08_conditionals/parity.py
@@ -0,0 +1,14 @@
+# Este código exemplifica o uso dos operadores aritméticos:
+# +, adição;
+# -, subtração;
+# *, multiplicação;
+# /, divisão;
+# %, módulo (resto da divisão).
+
+x = int(input("What's x? "))
+
+if x % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
