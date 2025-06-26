b0ab712635149ee874819d75450c2d225e1b9961
Fábio Soriano <fabio_vsr@yahoo.com.br>
2025-06-26 17:08:48 +0000

    Comando 'match'.

---
+++ 08_conditionals/match.py
@@ -0,0 +1,15 @@
+# Este código exemplifica o comando "match".
+# O comando "match" é o equivalente ao comando "switch" em outras linguagens.
+
+name = input("What's your name? ")
+
+if name == "Harry":
+    print("Gryffindor")
+elif name == "Hermione":
+    print("Gryffindor")
+elif name == "Ron":
+    print("Gryffindor")
+elif name == "Draco":
+    print("Slytherin")
+else:
+    print("Who?")
