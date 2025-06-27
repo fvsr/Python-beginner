f13028dd0c1e662600743f394b977e6d4830213d
Fábio Soriano <fabio_vsr@yahoo.com.br>
2025-06-27 06:07:41 +0000

    Loop for.

--- 09_loops/for.py
+++ 09_loops/for.py
@@ -30,6 +30,7 @@ for _ in range(3):
 # Se o usuário fornecer um valor positivo, o loop se interrompe,
 # mas se o usuário fornecer um valor negativo,
 # o programa vai pedir um valor novamente, até que um valor positivo seja inserido.
+"""
 while True:
     n = int(input("What's n? "))
     if n > 0:
@@ -37,5 +38,24 @@ while True:
 
 for _ in range(n):
     print("meow")
+"""
 
+# O mesmo código anterior, mas agora usando funções.
+# Aqui é o comando "return" que interrompe o loop, 
+# pois ele incorpora o comando "break", que também funciona em Python.
+# Aqui "return n" é obrigatório para enviar o valor que a função calculou. 
+def main():
+    number = get_number()
+    meow(number)
 
+def get_number():
+    while True:
+        n = int(input("What's n? "))
+        if n > 0:
+            return n
+
+def meow(n):
+    for _ in range(n):
+        print("meow")
+
+main()
