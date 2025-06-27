17a36776420f03c3b03c524d981e5c74ab2e70e5
Fábio Soriano <fabio_vsr@yahoo.com.br>
2025-06-27 05:44:12 +0000

    Loop for.

--- 09_loops/for.py
+++ 09_loops/for.py
@@ -1,4 +1,7 @@
-# O comando "for" também gera um loop, e faz controle de repetições.and
+
+# O "meow" é o miado de um gato americano, e geralmente ele mia mais de uma vez.
+
+# O comando "for" também gera um loop, e faz controle de repetições.
 
 # Os tipos de variáveis em Python vistos até agora são:
 # str, strings;
@@ -15,8 +18,24 @@ for i in [0,1,2]: # A lista é o "[0,1,2]".
 """
 
 # Usando a função "range()" para otimizar o código.
-""""""
-for _ in range(3): # Aqui o sinal "_" representa uma variável que não importa o nome,
-                   # porque não vai ser usada depois, nem recebe um valor anterior.
-                   # A função "range(3)" atribui os valores 0, 1 e 2 para essa variável.
+# Aqui o sinal "_" representa uma variável que não importa o nome,
+# porque não vai ser usada depois, nem recebe um valor anterior.
+# A função "range(3)" atribui os valores 0, 1 e 2 para essa variável.
+"""
+for _ in range(3): 
     print("meow")
+"""
+
+# Perguntando ao usuário quantas vezes o gato deve miar.
+# Se o usuário fornecer um valor positivo, o loop se interrompe,
+# mas se o usuário fornecer um valor negativo,
+# o programa vai pedir um valor novamente, até que um valor positivo seja inserido.
+while True:
+    n = int(input("What's n? "))
+    if n > 0:
+        break
+
+for _ in range(n):
+    print("meow")
+
+
