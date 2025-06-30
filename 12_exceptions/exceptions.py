b5adb0a44864e96c3aa095dd6442531855c442d4
Fábio Soriano <fabio_vsr@yahoo.com.br>
2025-06-30 18:25:31 +0000

    Exceptions

---
+++ 12_exceptions/exceptions.py
@@ -0,0 +1,17 @@
+# Exceções ocorrem quando aconteceu algum problema no código.
+# Para isso existem as mensagens de erro.
+
+# Vamos provocar um erro para vericar qual a mensagem de erro que é retornada.
+"""
+print("Hello, world!)
+"""
+# Foi retornada a seguinte mensagem de erro:
+# SyntaxError: unterminated string literal (detected at line 6)
+# Um erro de sintaxe quer dizer que o problema está no código que está escrito,
+# na forma que foi escrito.
+# Não terminada significa que não foi encerrada corretamente,
+# string é uma sequência de caracteres,
+# e literal quer dizer que não é uma variável, 
+# é algo que foi digitado letra por letra dentro do código.
+
+
