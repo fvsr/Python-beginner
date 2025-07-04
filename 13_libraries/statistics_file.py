8221f9df3b42c64abf1f386d976eb41101187c24
Fábio Soriano <fabio_vsr@yahoo.com.br>
2025-07-04 15:23:57 +0000

    Biblioteca statistics.

--- 13_libraries/statistics_file.py
+++ 13_libraries/statistics_file.py
@@ -1,12 +1,10 @@
 # Comandos "import" e "from", para bibliotecas e funções,
 # biblioteca "statistics" e funções "mean",
 
+# Este arquivo de código não pode ter o mesmo nome que uma biblioteca Python,
+# caso contrário as funções "import" e "from" vão buscar funções neste arquivo e não vão encontrar.
+
 # A função "mean" calcula a média pode receber um lista como argumento.
-"""
 import statistics
 
 print(statistics.mean([100, 90]))
-"""
-
-import sys
-print(sys.version)
