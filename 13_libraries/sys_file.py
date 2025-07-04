d83ab179a019094a8f7b81ff357b2da7200069f1
Fábio Soriano <fabio_vsr@yahoo.com.br>
2025-07-04 15:26:22 +0000

    Biblioteca sys

---
+++ 13_libraries/sys_file.py
@@ -0,0 +1,5 @@
+# Comandos "import" e "from", para bibliotecas e funções,
+# biblioteca "sys".
+
+# Este arquivo de código não pode ter o mesmo nome que uma biblioteca Python,
+# caso contrário as funções "import" e "from" vão buscar funções neste arquivo e não vão encontrar.
